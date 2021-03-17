9c93b42c8cac9b65fea30b226885a4f5455235d4
Justin Priest <justin.priest@alaska.gov>
2021-03-16 16:44:42 -0800

    Added age data Began outline of the script

---
+++ analysis/1_demoscript_cohoages.R
@@ -0,0 +1,14 @@
+# GitHub for Fisheries Scientists
+# Justin Priest
+# justin.priest@alaska.gov
+
+# March 18, 2021
+
+#####      DEMO SCRIPT      ####
+# Data are from ADF&G Coho Collections
+# Shaul et al. 2019
+
+
+library(tidyverse)
+library(lubridate)
+
