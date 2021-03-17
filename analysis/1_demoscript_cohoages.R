0c3883c642b725d532ad8bfcd2165bfd027ed288
Justin Priest <justin.priest@alaska.gov>
2021-03-16 18:05:26 -0800

    added more info about data background

--- analysis/1_demoscript_cohoages.R
+++ analysis/1_demoscript_cohoages.R
@@ -1,16 +1,21 @@
 # GitHub for Fisheries Scientists
+# March 18, 2021
 # Justin Priest
 # justin.priest@alaska.gov
 
-# March 18, 2021
+
 
 #####      DEMO SCRIPT      ####
 # Data are from ADF&G Coho Collections
 # Shaul et al. 2019
+# Stock Status and Review of Factors Affecting Coho Salmon 
+#   Returns and Escapements in Southeast Alaska
+# http://www.adfg.alaska.gov/FedAidPDFs/RIR.1J.2019.12.pdf
 
 
 library(tidyverse)
 library(lubridate)
+
 
 read_csv("data/ADFG_smoltages_AukeBernersHughSmith.csv") %>%
   mutate(Date = ymd(as.POSIXct(Date, format = "%m/%d/%Y", tz = "US/Alaska"))) %>%
