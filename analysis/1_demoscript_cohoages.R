db0d2e677216fe6533630f8299bb64343813ec22
Justin Priest <justin.priest@alaska.gov>
2021-03-16 16:58:13 -0800

    updated script to import data

--- analysis/1_demoscript_cohoages.R
+++ analysis/1_demoscript_cohoages.R
@@ -12,3 +12,12 @@
 library(tidyverse)
 library(lubridate)
 
+read_csv("data/ADFG_smoltages_AukeBernersHughSmith.csv") %>%
+  mutate(Date = ymd(as.POSIXct(Date, format = "%m/%d/%Y", tz = "US/Alaska"))) %>%
+  rename("location" = "Location",
+         "collectiondate" = "Date",
+         "fw_age" = "Age",
+         "length_mm" = "Length") %>%
+  mutate(location = replace(location, location == "AL", "Auke Lake"),
+         location = replace(location, location == "BR", "Berners River"),
+         location = replace(location, location == "HS", "Hugh Smith Lake"))
